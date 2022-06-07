1a26931c2f1881b3b4fa0f94bbf31bdbea6a1fd9
Vitor Carvalho <vitor.velozo2003@gmail.com>
2022-06-07 18:34:08 -0300

    Add Singleton

--- 1-Factory.py
+++ 1-Factory.py
@@ -3,33 +3,18 @@ from abc import ABC, abstractmethod
 
 
 class Creator(ABC):
-    """
-    The Creator class declares the factory method that is supposed to return an
-    object of a Product class. The Creator's subclasses usually provide the
-    implementation of this method.
-    """
 
     @abstractmethod
     def factory_method(self):
-        """
-        Note that the Creator may also provide some default implementation of
-        the factory method.
-        """
         pass
 
     def some_operation(self) -> str:
-        """
-        Also note that, despite its name, the Creator's primary responsibility
-        is not creating products. Usually, it contains some core business logic
-        that relies on Product objects, returned by the factory method.
-        Subclasses can indirectly change that business logic by overriding the
-        factory method and returning a different type of product from it.
-        """
+        # . . .
 
-        # Call the factory method to create a Product object.
+        # Call the factory method to create a Product object
         product = self.factory_method()
 
-        # Now, use the product.
+        # Now, use the product
         result = f"Creator: The same creator's code has just worked with {product.operation()}"
 
         return result
@@ -42,12 +27,6 @@ product's type.
 
 
 class ConcreteCreator1(Creator):
-    """
-    Note that the signature of the method still uses the abstract product type,
-    even though the concrete product is actually returned from the method. This
-    way the Creator can stay independent of concrete product classes.
-    """
-
     def factory_method(self) -> Product:
         return ConcreteProduct1()
 
@@ -58,11 +37,6 @@ class ConcreteCreator2(Creator):
 
 
 class Product(ABC):
-    """
-    The Product interface declares the operations that all concrete products
-    must implement.
-    """
-
     @abstractmethod
     def operation(self) -> str:
         pass
